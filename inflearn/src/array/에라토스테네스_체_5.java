a078f1079be5ca8b69a6d84cc8ccfac212b068ba
ìsoyounsoo <sohyounsoo@gmail.com>
2022-07-01 17:05:33 +0900

    07.01

--- inflearn/src/array/에라토스테네스_체_5.java
+++ inflearn/src/array/에라토스테네스_체_5.java
@@ -1,10 +1,29 @@
 package array;
 
+import java.util.Scanner;
+
 public class 에라토스테네스_체_5 {
 
+	public int solution(int n) {
+		int answer = 0;
+		
+		int[] ch = new int[n+1];
+		for(int i=2; i<=n; i++) {
+			if(ch[i] == 0) {
+				answer++;
+				for(int j=i; j<=n; j=j+i) ch[j] = 1;
+			}
+		}
+		
+		return answer;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		에라토스테네스_체_5 T = new 에라토스테네스_체_5();
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(T.solution(n));
 	}
 
 }
