9c426df4b9c6a53f3c317bfe4deafd558d652b1b
SoHyounSoo <sohyounsoo@gmail.com>
2022-07-19 20:55:14 +0900

    07.19

--- inflearn/src/soring_searching/삽입정렬.java
+++ inflearn/src/soring_searching/삽입정렬.java
@@ -11,6 +11,12 @@ import java.util.Scanner;
 public class 삽입정렬 {
 
 	public int[] solution(int n, int[] arr) {
+		for(int i=1; i<n; i++) {
+			int tmp = arr[i];
+			for(int j=i-1; j>=0; j--) {
+				if(arr[j]>tmp) arr[j+1] = arr[j];
+			}
+		}
 		
 		return arr;
 	}
