5de8fb8c64666ed94a2f2c54f8592f9c445c0a30
sohyounsoo <sohyounsoo@gmail.com>
2024-11-25 15:56:18 +0900

    피보나치 수열 다른 방법으로 풀이

--- inflearn/src/array/피보나치_수열_4.java
+++ inflearn/src/array/피보나치_수열_4.java
@@ -8,9 +8,22 @@ public class 피보나치_수열_4 {
 		int[] answer = new int[n];
 		answer[0] = 1;
 		answer[1] = 1;
+
 		for(int i=2; i<n; i++) {
 			answer[i] = answer[i-2] + answer[i-1];
 		}
+
+		return answer;
+	}
+
+	public int[] solution2(int n) {
+		int[] answer = new int[n];
+
+		for(int i=0; i<n; i++) {
+			if(i<=1) answer[i] = 1;
+			else answer[i] = answer[i-2] + answer[i-1];
+		}
+
 		return answer;
 	}
 	
@@ -21,6 +34,12 @@ public class 피보나치_수열_4 {
 		int n = sc.nextInt();
 		
 		for(int x : T.solution(n)) {
+			System.out.print(x + " ");
+		}
+
+		System.out.println();
+
+		for(int x : T.solution2(n)) {
 			System.out.print(x + " ");
 		}
 	}
