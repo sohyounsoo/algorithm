bd6b4215af006a91847e67af5cc86d1a70beb94f
So hyounsoo <sohyounsoo@gmail.com>
2025-02-01 15:41:17 +0900

    중복문자제거 set 활용 추가

--- inflearn/src/string/중복문자제거.java
+++ inflearn/src/string/중복문자제거.java
@@ -1,5 +1,6 @@
 package string;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class 중복문자제거 {
@@ -18,12 +19,26 @@ public class 중복문자제거 {
 		return answer;
 	}
 
+	public String solution2(String str) {
+		HashSet<Character> set = new HashSet<>();
+		StringBuilder result = new StringBuilder();
+
+		for(char c : str.toCharArray()) {
+			if(set.add(c)) {
+				result.append(c);
+			}
+
+		}
+		return result.toString();
+	}
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.next(); // ksekkset
 		중복문자제거 T = new 중복문자제거();
 		
-		System.out.print(T.solution(str));
+//		System.out.print(T.solution(str));
+		System.out.print(T.solution2(str));
 	}
 
 }
