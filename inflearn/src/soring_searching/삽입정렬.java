762558833f44c23000746547e346bf2692195aeb
So hyounsoo <sohyounsoo@gmail.com>
2025-02-08 12:15:31 +0900

    삽입 정렬 구현

--- inflearn/src/soring_searching/삽입정렬.java
+++ inflearn/src/soring_searching/삽입정렬.java
@@ -12,12 +12,13 @@ public class 삽입정렬 {
 
 	public int[] solution(int n, int[] arr) {
 		for(int i=1; i<n; i++) {
-			int tmp = arr[i];
-			for(int j=i-1; j>=0; j--) {
+			int tmp = arr[i], j;
+			for(j=i-1; j>=0; j--) {
 				if(arr[j]>tmp) arr[j+1] = arr[j];
+				else break;
 			}
+			arr[j+1] = tmp;
 		}
-		
 		return arr;
 	}
 
